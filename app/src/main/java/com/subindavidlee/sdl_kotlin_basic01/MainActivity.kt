8a03a127ad7b837b26dd51d00e39da491774ebc1
sdl-adhd-coder <96659361+sdl-adhd-coder@users.noreply.github.com>
2022-01-06 23:43:17 +0900

    log.e, log.d, Toast 기능까지 추가했음

--- app/src/main/java/com/subindavidlee/sdl_kotlin_basic01/MainActivity.kt
+++ app/src/main/java/com/subindavidlee/sdl_kotlin_basic01/MainActivity.kt
@@ -2,6 +2,8 @@ package com.subindavidlee.sdl_kotlin_basic01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +13,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        clickBtn.setOnClickListener{
+
+            //주석
+            Log.d("메인화면로그","클릭용버튼 눌림")
+            Log.e("메인화면로그","에러관련로그")
+
+        }
+
+        smallBtn.setOnClickListener{
+            Toast.makeText(this, "작은 버튼 눌림", Toast.LENGTH_SHORT).show()
+
+        }
 
 
     }
